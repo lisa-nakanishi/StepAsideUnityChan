4bfd93d586221045cae4c7bb2e405f460238595c
lisa-nakanishi <iwarehiko3@gmail.com>
2021-04-30 19:11:11 +0900

    Initial commit

---
+++ Assets/DestroyGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyGenerator : MonoBehaviour
+{
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+    //Unityちゃんと壁の距離
+    private float difference;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+        //Unityちゃんと壁の位置（ｚ座標）の差を求める
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //障害物を通り過ぎたとき破棄
+        if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag" || other.gameObject.tag == "CoinTag")
+        {
+
+            Destroy(other.gameObject);
+            
+
+        }
+    }
+
+
+}
